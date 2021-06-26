b0815d47f20ec1ec1e2e4f8a657f202426f4a968
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-06-26 03:47:08 +0200

    getters et setters in client file

--- JavaSource/societe/Client.java
+++ JavaSource/societe/Client.java
@@ -27,11 +27,56 @@ public class Client {
 		this.telephone = telephone;
 	}
 	
+	
 	/*
 	 * le constructeur vide du client
 	 */
 	
 	public Client(){
 		
+	}
+
+	/*
+	 * generation auto des getters et setters
+	 */
+	
+	public int getClientID() {
+		return clientID;
+	}
+
+	public void setClientID(int clientID) {
+		this.clientID = clientID;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
 	}
 }
