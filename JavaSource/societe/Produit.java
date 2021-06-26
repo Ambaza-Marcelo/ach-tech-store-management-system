04dbe4ecaf19321470362f98f9d5d630c3b24f1a
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-06-26 03:02:54 +0200

    les attributs de la classe Produit

--- JavaSource/societe/Produit.java
+++ JavaSource/societe/Produit.java
@@ -2,4 +2,13 @@ package societe;
 
 public class Produit {
 
+	/*
+	 * les attributs
+	 */
+	private int produitID;
+	private String nom;
+	private int prix_unitaire;
+	private int quantite;
+	
+	
 }
