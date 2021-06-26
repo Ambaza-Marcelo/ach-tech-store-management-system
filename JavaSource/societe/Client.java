811e53179349153694a139832efcd49ab6bdd6b0
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-06-26 02:52:59 +0200

    classe Client

--- JavaSource/societe/Client.java
+++ JavaSource/societe/Client.java
@@ -1,5 +1,13 @@
 package societe;
 
 public class Client {
-
+	/*
+	 * les attributs
+	 */
+	private int clientID;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private int telephone;
+	
 }
