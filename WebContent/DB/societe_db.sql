80f32ed9f8bbf0e24c93ee33a7c7ccc9a7507fc2
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-06-26 03:14:53 +0200

    quelque modification sur la societe_db

--- WebContent/DB/societe_db.sql
+++ WebContent/DB/societe_db.sql
@@ -6,21 +6,21 @@ create table client(
 	prenom varchar(20),
 	adresse varchar(100),
 	telephone varchar(15)
-);
+)ENGINE="innodb";
 
 create table produit(
 	produitID int primary key auto_increment,
 	nom varchar(50),
 	prix_unitaire int,
 	quantite int
-);
+)ENGINE="innodb";
 
 create table achat(
 	achatID int primary key auto_increment,
 	description varchar(255)
-);
+)ENGINE="innodb";
 
 create table vente(
 	venteID int primary key auto_increment,
 	description varchar(255)
-);
+)ENGINE="innodb";
